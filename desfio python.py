d1a58d2722218c19356e958c7b58b800975bae16
diocode01 <78504640+diocode01@users.noreply.github.com>
2021-04-25 09:02:02 -0300

    Create desfio python.py

---
+++ desfio python.py
@@ -0,0 +1,36 @@
+fron random import randint
+lista = list()
+cont = 0
+while True:
+    num = randint(1,60)
+     if num not in lista:
+       lista.append(num)
+       cont +=1
+     if cont >= 6:
+       break
+lista.sort()      
+print(lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
